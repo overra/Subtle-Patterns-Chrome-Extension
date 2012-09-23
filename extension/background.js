2db02909cdb134c75906b6dbbc18b78a02f7df47
overra <overra@gmail.com>
2012-09-23 18:38:01 -0500

    Initial commit

---
+++ extension/background.js
@@ -0,0 +1,34 @@
+var raw_url  = 'https://raw.github.com/subtlepatterns/SubtlePatterns/master/',
+    patterns = [],
+    page     = 0;
+
+chrome.extension.onMessage.addListener(function(message, info, cb) {
+    if (message == 'getPatterns') {
+        cb({
+            patterns: patterns,
+            page:     page 
+        });
+    }
+    else if (message.pattern) {
+        chrome.tabs.insertCSS({
+                code: 'body { background-image: url(' + raw_url + message.pattern + ') !important; }'
+        });
+    }
+    else if (message.page) {
+        page = message.page;
+    }
+});
+
+function loadPatterns(j) {
+    var file;
+    for (var i in j.data) {
+        file = j.data[i];
+        if (/\.png$/.test(file.name)) {
+            patterns.push(file);
+        }
+    }
+}
+
+var patternsJSON = document.createElement('script');
+patternsJSON.src = 'https://api.github.com/repos/subtlepatterns/SubtlePatterns/contents/?callback=loadPatterns'
+document.body.appendChild(patternsJSON);
---
+++ extension/popup.js
@@ -0,0 +1,64 @@
+var raw_url = 'https://raw.github.com/subtlepatterns/SubtlePatterns/master/',
+    patterns_div = document.getElementById('patterns'),
+    name_div = document.getElementById('name'),
+    next_btn = document.getElementById('next'),
+    prev_btn = document.getElementById('prev'),
+    patterns,
+    page;
+
+function loadPatterns() {
+
+    var pattern, 
+        file,
+        start = page * 12,
+        end   = start + 12;
+
+    if (page===0) {
+        prev_btn.setAttribute('disabled', 'disabled');
+    }
+    else {
+        prev_btn.removeAttribute('disabled');
+    }
+
+    while (patterns_div.hasChildNodes()) {
+        patterns_div.removeChild(patterns_div.firstChild);
+    }
+
+    for (var i = start, x; i < end; i++) {
+        file = patterns[i];
+        x = i%12;
+        pattern = document.createElement('div');
+        pattern.className = 'pattern';
+        pattern.style.backgroundImage = 'url(' + raw_url + file.name + ')';
+        pattern.style.top = ((x - x%4) / 4) * 65;
+        pattern.style.left = (x%4) * 65;
+        pattern.setAttribute('data-name', file.name);
+        pattern.onmouseover = function() {
+            name_div.innerText = this.getAttribute('data-name');
+        };
+        pattern.onmouseout = function() {
+            name_div.innerText = '';
+        };
+        pattern.onclick = function() {
+            chrome.extension.sendMessage({pattern: this.getAttribute('data-name')});
+        };
+        patterns_div.appendChild(pattern);
+    }
+}
+
+prev_btn.onclick = function() {
+    page--;
+    loadPatterns();
+    chrome.extension.sendMessage({page: page});    
+};
+next_btn.onclick = function() {
+    page++;
+    loadPatterns();
+    chrome.extension.sendMessage({page: page});    
+};
+
+chrome.extension.sendMessage('getPatterns', function(p) {
+    patterns = p.patterns;
+    page = p.page;
+    loadPatterns();
+});
